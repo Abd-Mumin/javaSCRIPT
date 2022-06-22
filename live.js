10b1b666583a0231b8a9938d898727d67aa8dca5
Abd-Mumin <ajiboyemumin99@gmail.com>
2022-06-22 03:00:14 +0100

    javascript alerts

---
+++ live.js
@@ -0,0 +1,3 @@
+// To print out name, height, and country on my screen>>>>>>>>>>>>>>>>>>>>>>>>>
+
+alert("Hello! My name is Ajiboye Abdulmumin,\n\nI am 7ft tall in height,\n\nand I am a Nigerian.");
